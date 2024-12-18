238fc8ae0add0d3fd6fd7a049ab08513125ee9a8
Jemisson <jemison321@gmail.com>
2024-12-18 10:38:59 -0400

    Revert "adiciona login de usuário e define cookies"
    This reverts commit 9a36ca7d30e7d9fdc4a70f0de18bba9a4af6221a.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import PrivateRoute from "./components/PrivateRoute";
 import Navbar from "./components/Navbar";
 import ProductCatalog from "./components/ProductCatalog";
-import Login from "./pages/Login";
-import AdminPage from "./pages/AdminPage";
 
-function App() {
+function App () {
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
 
   const handleCategoryClick = (categoryId: string | null) => {
@@ -15,39 +10,14 @@ function App() {
   };
 
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          {/* Rota Pública: Página Principal */}
-          <Route
-            path="/"
-            element={
-              <>
-                <Navbar
-                  onCategoryClick={handleCategoryClick}
-                  activeCategory={selectedCategory}
-                />
-                <ProductCatalog selectedCategory={selectedCategory} />
-              </>
-            }
-          />
-
-          {/* Rota Pública: Página de Login */}
-          <Route path="/login" element={<Login />} />
-
-          {/* Rota Protegida: Área Administrativa */}
-          <Route
-            path="/admin"
-            element={
-              <PrivateRoute>
-                <AdminPage />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <>
+       <Navbar 
+        onCategoryClick={handleCategoryClick}
+       activeCategory={selectedCategory} 
+      />
+       <ProductCatalog selectedCategory={selectedCategory} />
+    </>
   );
-}
+};
 
 export default App;
