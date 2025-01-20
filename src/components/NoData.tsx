421a97468755b5e705737a2f1740820a01813d1f
Jemisson <jemison321@gmail.com>
2025-01-20 18:30:17 -0400

    Adiciona not found para produyctCatalog

---
+++ src/components/NoData.tsx
@@ -0,0 +1,10 @@
+
+function NoData() {
+  return (
+    <div className="flex flex-col items-center justify-center mt-10">
+      <img src="/notFound.svg" alt="Nenhum dado encontrado" className="w-auto h-auto" />
+    </div>
+  );
+};
+
+export default NoData;
