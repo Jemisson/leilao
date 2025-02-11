750a7da3ef83a7d66d75250ccbe7c33ae6efc932
Jemisson <jemison321@gmail.com>
2025-02-11 13:07:25 -0400

    Ajuste para WebSocket

--- src/contexts/WebSocketProvider.tsx
+++ src/contexts/WebSocketProvider.tsx
@@ -3,11 +3,13 @@ import ActionCable from "actioncable";
 import { WebSocketContext } from "./WebSocketContext";
 import { WebSocketContextProps } from "../types";
 
+const WEBSOCKET_URL = import.meta.env.VITE_WEBSOCKET_URL
+
 export const WebSocketProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [cable, setCable] = useState<WebSocketContextProps["cable"]>(null);
 
   useEffect(() => {
-    const ws = ActionCable.createConsumer("ws://localhost:3000/cable");
+    const ws = ActionCable.createConsumer(WEBSOCKET_URL);
     setCable(ws);
 
     return () => {
