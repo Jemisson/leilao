0727b06916a157c78ab2ad0155ab06a38dba813d
Jemisson <jemison321@gmail.com>
2025-04-02 14:59:27 -0400

    Altera dominio da api

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 const API_BASE_URL = process.env.NODE_ENV === "production"
-  ? "https://api_staging_leilao.codenova.com.br"
+  ? "https://apileilao.codenova.com.br"
   : "http://localhost:3000";   
 
 export default defineConfig({
